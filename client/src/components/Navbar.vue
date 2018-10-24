58902271335aa714fff319bf5870e9fac4d7f335
gizmo49 <chikacheezy@gmail.com>
2018-10-24 20:19:43 +0100

    still working on this

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+    <div class="container">
+        <span class="navbar-brand">Shopping Cart</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav  ml-auto">
+                <li class="nav-item active" v-for="category in categories" :key="category._id">
+                   {{ category.title }}
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+import Api from '@/services/Api'
+export default {
+    data() {
+        return {
+            categories: []
+        }
+    },
+    created() {
+        Api().get('/categories')
+            .then(response => {
+                this.categories = response.data;
+            });
+            
+    }, 
+    computed: {
+        cart() {
+            return this.$store.getters.getCart
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,6 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
